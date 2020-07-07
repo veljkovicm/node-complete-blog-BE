ccaccbe7d77eb73bcfae742ce45c8346077cac24
Milan <comiv92@gmail.com>
2020-07-07 13:47:32 +0200

    adds getPost method

--- controllers/feed.js
+++ controllers/feed.js
@@ -46,3 +46,22 @@ exports.createPost = (req, res, next) => {
     next(err);
   });;
 };
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+  .then(post => {
+    if(!post) {
+      const error = new Error('Could not find post.');
+      error.statusCode = 404;
+      throw error;
+    }
+    res.status(200).json({ message: 'Post fetched.', post: post })
+  })
+  .catch(err => {
+    if(!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  });;
+};
--- routes/feed.js
+++ routes/feed.js
@@ -18,4 +18,6 @@ router.post('/post', [
     .isLength({min: 5}),
 ], feedController.createPost);
 
+router.get('/post/:postId', feedController.getPost);
+
 module.exports = router;
