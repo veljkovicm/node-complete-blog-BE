63b7d76bd83b98c984ee3cbfdb07b6d611aef70b
Milan <comiv92@gmail.com>
2020-07-07 16:01:30 +0200

    Adds the updatePost method

--- routes/feed.js
+++ routes/feed.js
@@ -9,15 +9,32 @@ const router = express.Router();
 router.get('/posts', feedController.getPosts);
 
 // POST /feed/post
-router.post('/post', [
-  body('title')
-    .trim()
-    .isLength({min: 5}),
-  body('content')
-    .trim()
-    .isLength({min: 5}),
-], feedController.createPost);
+router.post(
+  '/post',
+  [
+    body('title')
+      .trim()
+      .isLength({min: 5}),
+    body('content')
+      .trim()
+      .isLength({min: 5}),
+  ],
+  feedController.createPost
+);
 
 router.get('/post/:postId', feedController.getPost);
+
+router.put(
+  '/post/:postId',
+  [
+    body('title')
+      .trim()
+      .isLength({min: 5}),
+    body('content')
+      .trim()
+      .isLength({min: 5}),
+  ],
+  feedController.updatePost
+);
 
 module.exports = router;
