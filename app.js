f430a59ce3209a2ee7faf6f992bab385eaf329a2
Milan <comiv92@gmail.com>
2020-07-07 13:31:53 +0200

    adds images route and error middleware

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -7,8 +9,9 @@ const feedRoutes = require('./routes/feed');
 
 const app = express();
 
-app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -17,7 +20,16 @@ app.use((req, res, next) => {
   next();
 });
 
+
+
 app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({message: message});
+});
 
 mongoose.connect(config.MONGODB_URI)
 .then(result => {
