22cf474414764aef371be8e9eb448cdc773c438b
Milan <comiv92@gmail.com>
2020-07-07 12:40:34 +0200

    Updates /post validation

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,5 @@
+const { validationResult } = require('express-validator/check');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [{
@@ -8,12 +10,21 @@ exports.getPosts = (req, res, next) => {
       creator: {
         name: 'John Smith'
       },
-      createdAt: new Date()
+      createdAt: new Date(),
     }]
   });
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(422)
+      .json({
+        message: 'Validation failed, entered data is incorrect.',
+        errors: errors.array(),
+      })
+  }
   const { title, content } = req.body;
   // Create post in db
   res.status(201).json({
