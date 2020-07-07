dd5230e867a4938f2e308a556b2ec104791a4bbc
Milan <comiv92@gmail.com>
2020-07-07 12:06:46 +0200

    Updates the getPosts method

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,8 +1,14 @@
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [{
+      _id: '1',
       title: 'First Post',
-      content: 'This is the first post!'
+      content: 'This is the first post!',
+      imageUrl: 'images/legcat.jpg',
+      creator: {
+        name: 'John Smith'
+      },
+      createdAt: new Date()
     }]
   });
 };
