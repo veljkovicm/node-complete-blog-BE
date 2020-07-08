e2cff3a1f41407256991991cb206c77902643b0b
Milan <comiv92@gmail.com>
2020-07-08 11:45:29 +0200

    Adds pagination logic

--- controllers/feed.js
+++ controllers/feed.js
@@ -4,18 +4,34 @@ const path = require('path');
 const { validationResult } = require('express-validator/check');
 
 const Post = require('../models/post');
+const { CLIENT_RENEG_LIMIT } = require('tls');
 
 exports.getPosts = (req, res, next) => {
-  Post.find()
-  .then(posts => {
-    res.status(200).json({ message: 'Fetched posts successfully.', posts: posts})
-  })
-  .catch(err => {
-    if(!err.statusCode) {
-      err.statusCode = 500;
-    }
-    next(err);
-  });
+  const currentPage = req.query.page || 1;
+  const perPage = 2;
+  let totalItems;
+  Post
+    .find()
+    .countDocuments()
+    .then(count => {
+      totalItems = count;
+      return Post.find()
+        .skip((currentPage - 1) * perPage)
+        .limit(perPage);
+    })
+    .then(posts => {
+      res.status(200).json({
+        message: 'Fetched posts successfully.',
+        posts: posts,
+        totalItems: totalItems 
+      })
+    })
+    .catch(err => {
+      if(!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
 
 exports.createPost = (req, res, next) => {
