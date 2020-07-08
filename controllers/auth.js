4bb15e7b281650ad8564bfdf01bda849ed073b8d
Milan <comiv92@gmail.com>
2020-07-08 12:22:36 +0200

    Adds user signup flow

---
+++ controllers/auth.js
@@ -0,0 +1,37 @@
+const { validationResult } = require('express-validator/check');
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+
+exports.signup = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed.');
+    error.statusCode = 422;
+    error.data = errors.array();
+    throw error;
+  }
+  const {
+    email,
+    name,
+    password
+  } = req.body;
+  bcrypt
+    .hash(password, 12)
+    .then(hashedPassword => {
+      const user = new User({
+        email: email,
+        password: hashedPassword,
+        name: name,
+      });
+      return user.save()
+    })
+    .then(result => {
+      res.status(201).json({ message: 'User created!', userId: result._id });
+    })
+    .catch(err => {
+      if(!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
