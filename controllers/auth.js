5f3c9ffdd16aa6067a5b6f9c470bba30bdea0283
Milan <comiv92@gmail.com>
2020-07-09 13:29:52 +0200

    pass user email to FE, on login

--- controllers/auth.js
+++ controllers/auth.js
@@ -65,7 +65,7 @@ exports.login = (req, res, next) => {
         'somesupersecretsecretkey',
         { expiresIn: '1h' }
       );
-      res.status(200).json({ token: token, userId: loadedUser._id.toString() });
+      res.status(200).json({ token: token, userId: loadedUser._id.toString(), email: loadedUser.email });
     })
     .catch(err => {
       if(!err.statusCode) {
