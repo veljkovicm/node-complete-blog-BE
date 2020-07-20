205d73ca95e34b4a43dd0a9132012646d2dae09c
Milan <comiv92@gmail.com>
2020-07-20 17:25:11 +0200

    Fixes auth unit test

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -35,7 +35,8 @@ describe('Auth middleware', function() {
     jwt.verify.returns({ userId: 'abc' });
     authMiddleware(req, {}, () => {});
     expect(req).to.have.property('userId');
-    expect(req).to.have.property( be.true;
+    expect(req).to.have.property('userId', 'abc');
+    expect(jwt.verify.called).to.be.true;
     jwt.verify.restore();
   });
 
