deff4ee591f02370fa557b628a52692fcd1d50d7
Milan <comiv92@gmail.com>
2020-07-20 13:18:01 +0200

    Fixes error handling in auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,20 +3,22 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
   const authHeader = req.get('Authorization');
   if (!authHeader) {
-    req.isAuth = false;
-    return next();
+    const error = new Error('Not authenticated.');
+    error.statusCode = 401;
+    throw error;
   }
   const token = authHeader.split(' ')[1];
   let decodedToken;
   try {
     decodedToken = jwt.verify(token, 'somesupersecretsecretkey')
   } catch (err) {
-    req.isAuth = false;
-    return next();
+    err.statusCode = 500;
+    throw err;
   }
   if (!decodedToken) {
-    req.isAuth = false;
-    return next();
+    const error = new Error('Not authenticated.');
+    error.statusCode = 401;
+    throw error;
   }
   req.userId = decodedToken.userId;
   req.isAuth = true;
